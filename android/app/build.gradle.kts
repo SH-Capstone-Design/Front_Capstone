1940840a89494c05d187f7fa798d3b1f0146a264
dbehdghks <rdwhan@naver.com>
2025-08-06 10:42:41 +0900

    구ê¸android 구글 로그인ã 완료ã

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.connectbeat"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -33,7 +33,48 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debplugins {
+            //    id("com.android.application")
+            //    id("kotlin-android")
+            //    id("dev.flutter.flutter-gradle-plugin")
+            //}
+            //
+            //android {
+            //    namespace = "com.example.connectbeat"
+            //    compileSdk = 35
+            //    ndkVersion = "27.0.12077973"
+            //
+            //    compileOptions {
+            //        sourceCompatibility = JavaVersion.VERSION_11
+            //        targetCompatibility = JavaVersion.VERSION_11
+            //    }
+            //
+            //    kotlinOptions {
+            //        jvmTarget = JavaVersion.VERSION_11.toString()
+            //    }
+            //
+            //    defaultConfig {
+            //        applicationId = "com.example.connectbeat"
+            //        minSdk = 21
+            //        targetSdk = 35
+            //        versionCode = 1
+            //        versionName = "1.0"
+            //    }
+            //
+            //    buildTypes {
+            //        release {
+            //            signingConfig = signingConfigs.getByName("debug")
+            //        }
+            //    }
+            //}
+            //
+            //flutter {
+            //    source = "../.."
+            //}
+            //
+            //dependencies {
+            //    implementation("com.google.android.gms:play-services-auth:20.5.0")
+            //}ug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
